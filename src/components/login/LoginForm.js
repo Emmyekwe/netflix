e0fa7ce4b1e0733d244d37a6efbbff0f6e7bc4d1
Emmanuel Ukhueleigbe <emmanuel@chekkitapp.com>
2021-04-27 11:23:13 +0100

    net

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import FBlogo from '../../images/fb-logo.png';
 
-const regexp = RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
+const regexp = RegExp(/^([a-zA-Z0-9_\]+)@([a-zA-Z0-9_\]+)([a-zA-Z]{2,5})$/);
 
 
 const initState = {
